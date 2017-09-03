354b97c959ad52b7f3dbcdd515f4f842af47160a
Subhangi <subhangiagarwala@gmail.com>
2017-09-03 15:23:24 -0400

    Assignment 2

--- src/test/java/api/CreateReceiptRequestTest.java
+++ src/test/java/api/CreateReceiptRequestTest.java
@@ -41,4 +41,13 @@ public class CreateReceiptRequestTest {
         validator.validate(receipt);
         assertThat(validator.validate(receipt), hasSize(1));
     }
+
+    @Test
+    public void testConflicts() {
+        CreateReceiptRequest receipt = new CreateReceiptRequest();
+        receipt.merchant = "OK";
+
+        receipt.amount = new Integer(4);
+        assertThat(validator.validate(receipt), empty());
+    }
 }
